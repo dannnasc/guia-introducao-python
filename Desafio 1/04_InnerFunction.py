7f12f17184522624738677340d6a198247ec96fa
daniel <daninascimento120@gmail.com>
2023-05-06 17:13:02 +0000

    04_InnerFunction.py

--- Desafio 1/04_InnerFunction.py
+++ Desafio 1/04_InnerFunction.py
@@ -1,3 +1,8 @@
 # Criar uma função externa que irá aceitar dois parametros, a e b
 # Crie uma função interna dentro da função externa que irá retornar a soma os parametros a e b 
 # Na função externa, adicione 5 ao retorno da funcao interna e escreva na tela este valor
+def ext(a,b):
+    def int(a,b):
+        c=a+b
+        return c
+    return int(a,b)+5
