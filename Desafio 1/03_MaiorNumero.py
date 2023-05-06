ab474a00d526914f467595f2496c25bc495ef4d7
daniel <daninascimento120@gmail.com>
2023-05-06 17:07:37 +0000

    01_Operacoes.py

--- Desafio 1/03_MaiorNumero.py
+++ Desafio 1/03_MaiorNumero.py
@@ -1,5 +1,9 @@
 # Escreva uma função para encontrar o maior entre 3 numeros
 
 def Max(a,b,c):
-    
-    return 0
+    if a>b and a>c:
+        maior=a
+    if b>a and b>c: 
+        maior =b
+    maior=c
+    return maior
